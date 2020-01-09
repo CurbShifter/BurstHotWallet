9f13e041f8295f7380e8be094cefdcc1a7d34af5
Curb <40964880+CurbShifter@users.noreply.github.com>
2020-01-09 05:33:04 +0100

    rev nr update

--- Source/Version.h
+++ Source/Version.h
@@ -2,10 +2,10 @@
 #ifndef __PROJECT_SubWCRev__
 #define __PROJECT_SubWCRev__
 
-#define PROJECT_SVNRevisionN 32
-#define PROJECT_SVNRevision "32"
+#define PROJECT_SVNRevisionN 33
+#define PROJECT_SVNRevision "33"
 #define PROJECT_SVNModified "Modified"
-#define PROJECT_SVNDate     "2020/01/05 19:55:13"
+#define PROJECT_SVNDate     "2020/01/09 03:51:49"
 #define PROJECT_SVNRange    "WCRANGE$"
 #define PROJECT_SVNMixed    "Mixed revision"
 #define PROJECT_SVNURL      "https://github.com/CurbShifter/BurstHotWallet.git/trunk"
